aeacc64e31da12a3f76723f20706d46ba5b78ccc
Saksham S <sakshamsrivastava034060@gmail.com>
2025-07-11 15:57:00 +0530

    Initial Commit 1.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,6 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        'hero-radial': 'radial-gradient(circle at 20% 35%,rgb(17, 35, 80) 0%, #050B1D 40%, transparent 60%)',
-      },
       keyframes: {
         fadeIn: {
           '0%': { opacity: 0 },
