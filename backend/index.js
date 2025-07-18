b9237d8bdeb8bd5de327469258716ce88d693da5
Saksham S <sakshamsrivastava034060@gmail.com>
2025-07-18 20:28:52 +0530

    Backend Gitignore Commit

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,12 @@ const cors = require("cors");
 const axios = require("axios");
 const app = express();
 const PORT = 3001;
+require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 
-const OPENROUTER_API_KEY = "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";
+const OPENROUTER_API_KEY = process.env.OPENROUTER_API_KEY;
 
 app.post("/chat", async (req, res) => {
   const { message } = req.body;
